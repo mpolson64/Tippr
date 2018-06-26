20c57312d089f338deb2cbc5493feba6a344fd47
Miles Olson <mpolson64@fb.com>
2018-06-26 13:14:41 -0700

    Initial Commit

---
+++ Tippr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tippr
+//
+//  Created by Miles Olson on 6/26/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tippr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tippr
+//
+//  Created by Miles Olson on 6/26/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
